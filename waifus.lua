b4786f19c349e2cd8049aabd25267146b7cc8801
argot <pablotesari@gmail.com>
2018-08-13 14:34:17 -0300

    local vars and func returns

--- waifus.lua
+++ waifus.lua
@@ -3,28 +3,14 @@ testvar = "uguu~"
 
 --REFER TO getWaifu.lua for possible rarities
 function initAllWaifus()
-    waifuArray = {}
-    AddWaifu(1, "ProgramingTrap", "Programing Trap", "Normal", true, "Learn programming while crossdressing!")
-        waifu1 = waifu
-        table.insert(waifuArray, waifu1)
-    AddWaifu(2, "SpaceElf", "Space Elf", "Normal", false, "~Enhanced~ Elves. Also, they possess augmentations.")
-        waifu2 = waifu
-        table.insert(waifuArray, waifu2)
-    AddWaifu(3, "Necron", "Necron Cutie", "Rare", false, "Kawaii undead metal")
-        waifu3 = waifu
-        table.insert(waifuArray, waifu3)
-    AddWaifu(4, "Bruno", "Disappointment Bruno", "Trash", false, "Used to serve on a starship. Now he just furiously masturbates to chinese cartoons")
-        waifu4 = waifu
-        table.insert(waifuArray, waifu4)
-    AddWaifu(5, "TsunTwo", "Tsundere Space Mewtwo", "Uncommon", false, "'The result of an alien Nazi eugenics programme basically'")
-        waifu5 = waifu
-        table.insert(waifuArray, waifu5)
-    AddWaifu(6, "Catgirl", "Generic Catgirl", "Normal", false, "Their DNA is made up of 1% carbon and 99% fanservice!")
-        waifu6 = waifu
-        table.insert(waifuArray, waifu6)
-    AddWaifu(7, "Unity", "Solidarity", "Rare", false, "Hivemind lifeform created in a lab by Canonical")
-        waifu7 = waifu
-        table.insert(waifuArray, waifu7)
+    local waifuArray = {}
+    table.insert(waifuArray, createWaifu(1, "ProgramingTrap", "Programing Trap", "Normal", true, "Learn programming while crossdressing!"))
+    table.insert(waifuArray, createWaifu(2, "SpaceElf", "Space Elf", "Normal", false, "~Enhanced~ Elves. Also, they possess augmentations."))
+    table.insert(waifuArray, createWaifu(3, "Necron", "Necron Cutie", "Rare", false, "Kawaii undead metal"))
+    table.insert(waifuArray, createWaifu(4, "Bruno", "Disappointment Bruno", "Trash", false, "Used to serve on a starship. Now he just furiously masturbates to chinese cartoons"))
+    table.insert(waifuArray, createWaifu(5, "TsunTwo", "Tsundere Space Mewtwo", "Uncommon", false, "'The result of an alien Nazi eugenics programme basically'"))
+    table.insert(waifuArray, createWaifu(6, "Catgirl", "Generic Catgirl", "Normal", false, "Their DNA is made up of 1% carbon and 99% fanservice!"))
+    table.insert(waifuArray, createWaifu(7, "Unity", "Solidarity", "Rare", false, "Hivemind lifeform created in a lab by Canonical"))
     
     
     --DEBUG: Read out root table contents
@@ -33,11 +19,11 @@ function initAllWaifus()
     --    print("DEBUG: Contents: ", v.id, v.name, v.unlocked)
     --end
     
-    
+    return waifuArray 
 end
 
-function AddWaifu(number, id, name, rarity, unlocked, bio)
-    waifu = {}
+function createWaifu(number, id, name, rarity, unlocked, bio)
+    local waifu = {}
     waifu.number = number
     waifu.id = id
     waifu.name = name
@@ -45,9 +31,8 @@ function AddWaifu(number, id, name, rarity, unlocked, bio)
     waifu.unlocked = unlocked
     waifu.bio = bio
     --print("DEBUG: Adding Waifu: ", number, id, name, unlocked)
+    
+    return waifu
 end
 
-
-initAllWaifus()
-
-
+waifuArray = initAllWaifus()
